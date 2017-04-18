67f11583daba7248ed3bd1fe3e7e68a18e4a1e14
Andi DeVois <adevois@gmail.com>
2017-04-18 18:30:38 -0500

    Changes

---
+++ ApiPlaygroundReDux/Controllers/SashController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiPlaygroundReDux.Controllers
+{
+    public class SashController : ApiController
+    {
+        public SashController()
+        {
+
+        }
+
+        public Sash GetSash(int Id)
+        {
+            return new Sash
+            {
+                Cost = 12,
+                Name = "Divorce Sash",
+                QuantityRemaining = 1
+            };
+        }
+    }
+
+    public class Sash
+    {
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+        public int QuantityRemaining { get; set; }
+    }
+
+}
